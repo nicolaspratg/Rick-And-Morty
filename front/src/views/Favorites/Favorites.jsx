1b42f4983911f79a5a936a924b8e2838384ed0b2
nicolaspratg <nicoprat@hotmail.com>
2024-01-19 11:07:43 -0300

    Finished css

--- front/src/views/Favorites/Favorites.jsx
+++ front/src/views/Favorites/Favorites.jsx
@@ -15,32 +15,36 @@ const Favorites = (e) => {
   };
   return (
     <div className={styles.bgFavs}>
-      <h2>My Favorites</h2>
-      <label>Order:</label>
-      <select onChange={handleOrder}>
-        <option value="A">Ascendente</option>
-        <option value="D">Descendente</option>
-      </select>
-      <label>Filter by Gender:</label>
-      <select onChange={handleFilter}>
-        <option value="Male">Male</option>
-        <option value="Female">Female</option>
-        <option value="Genderless">Genderless</option>
-        <option value="unknown">Unknown</option>
-      </select>
-      {myFavorites.map(({ id, name, species, gender, image, origin }) => {
-        return (
-          <Card
-            key={id}
-            id={id}
-            name={name}
-            species={species}
-            gender={gender}
-            origin={origin}
-            image={image}
-          />
-        );
-      })}
+      <div>
+        <div className={styles.filterSection}>
+          <h2>My Favorites</h2>
+          <select onChange={handleOrder}>
+            <option value="A">Ascendente</option>
+            <option value="D">Descendente</option>
+          </select>
+          <select onChange={handleFilter}>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Genderless">Genderless</option>
+            <option value="unknown">Unknown</option>
+          </select>
+        </div>
+        <div className={styles.cardContainer}>
+          {myFavorites.map(({ id, name, species, gender, image, origin }) => {
+            return (
+              <Card
+                key={id}
+                id={id}
+                name={name}
+                species={species}
+                gender={gender}
+                origin={origin}
+                image={image}
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
