895ed1c81d4f517a3691f79668443a49460bb72e
nicolaspratg <nicoprat@hotmail.com>
2024-01-24 20:44:39 -0300

    Refactored conditionals, now cleaner with ternary ops

--- back/src/index.js
+++ back/src/index.js
@@ -1,22 +1,11 @@
 const http = require("http");
-const data = require("./utils/data");
-
+const { getCharById } = require("./controllers/getCharById");
 http
   .createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.writeHead(200, { "Content-Type": "application/json" });
     const url = req.url;
-    if (url.includes("/rickandmorty/character")) {
-      const id = url.split("/").pop();
-      const character = data.find((character) => character.id == id);
-      if (!character) {
-        return res.end(
-          JSON.stringify([{ Mensaje: "No existe un character con ese id" }])
-        );
-      }
-      return res.end(JSON.stringify(character));
-    }
-
-    res.end("No hay chorizo");
+    url.includes("/rickandmorty/character")
+      ? getCharById(res, url.split("/").pop())
+      : res.end("Invalid route");
   })
   .listen(3001);
