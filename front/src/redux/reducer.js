f77c027f07007e9b033fea73ca4354516876584c
nicolaspratg <nicoprat@hotmail.com>
2024-01-29 17:01:40 -0300

    changed some reducers due to provided code on the integration HW, fixed order and filter

--- front/src/redux/reducer.js
+++ front/src/redux/reducer.js
@@ -8,43 +8,38 @@ const rootReducer = (state = initialState, action) => {
     case ADD_FAV:
       return {
         ...state,
-        myFavorites: [...state.myFavorites, action.payload],
-        allCharacters: [...state.allCharacters, action.payload],
+        myFavorites: action.payload,
+        allCharacters: action.payload,
       };
     case REMOVE_FAV:
-      const updatedFavorites = state.myFavorites.filter(
-        (character) => character.id !== action.payload
-      );
-      return {
-        ...state,
-        myFavorites: updatedFavorites,
-        allCharacters: updatedFavorites,
-      };
+      return { ...state, myFavorites: action.payload };
     case FILTER:
-      const filteredCharacters = state.allCharacters.filter(
-        (character) => character.gender === action.payload
-      );
-
-      return {
-        ...state,
-        myFavorites: filteredCharacters,
-        filteredGender: action.payload,
-      };
-    case ORDER:
-      const orderedFavorites = [...state.myFavorites];
-
-      if (action.payload === "A") {
-        orderedFavorites.sort((a, b) => a.id - b.id);
-      } else if (action.payload === "D") {
-        orderedFavorites.sort((a, b) => b.id - a.id);
+      if (action.payload === "all") {
+        return {
+          ...state,
+          myFavorites: [...state.allCharacters],
+        };
+      } else {
+        return {
+          ...state,
+          myFavorites: state.allCharacters.filter(
+            (fav) => fav.gender === action.payload
+          ),
+        };
       }
-
+    case ORDER:
+    case "ORDER":
       return {
         ...state,
-        myFavorites: orderedFavorites,
-        sortOrder: action.payload,
+        myFavorites: [...state.allCharacters].sort((a, d) => {
+          if (action.payload === "A") {
+            return a.id - d.id;
+          }
+          if (action.payload === "D") {
+            return d.id - a.id;
+          }
+        }),
       };
-
     default:
       return state;
   }
