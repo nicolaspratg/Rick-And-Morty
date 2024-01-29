9c0504e9947947f2fe9bf30766cfaa6dbd8b61c8
nicolaspratg <nicoprat@hotmail.com>
2024-01-29 17:11:34 -0300

    getChars

--- back/src/controllers/getCharById.js
+++ back/src/controllers/getCharById.js
@@ -1,25 +1,39 @@
 const axios = require("axios");
+const BASE_URL = "https://rym2.up.railway.app/api/character/";
+const API_KEY = "pi-nicolaspratg";
 
-module.exports.getCharById = (res, id) => {
+module.exports.getCharById = (req, res) => {
+  const { id } = req.params;
+  const URL = `${BASE_URL}${id}?key=${API_KEY}`;
+
   axios
-    .get(`https://rym2.up.railway.app/api/character/${id}?key=pi-nicolaspratg`)
-    .then((response) => {
-      const { id, name, gender, species, origin, image, status } =
-        response.data;
+    .get(URL)
+    .then(({ data }) => {
+      const { id, name, gender, species, origin, image, status } = data;
+
       const character = {
         id,
+        status,
         name,
-        gender,
         species,
         origin,
         image,
-        status,
+        gender,
       };
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(character));
+
+      res
+        .status(character.id ? 200 : 404)
+        .json(character.id ? character : { error: "Not Found" });
     })
     .catch((error) => {
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end(error.message);
+      const errorMessage = error.response
+        ? `Server responded with a non-2xx status code: ${error.response.status}`
+        : error.request
+        ? "No response received from the server"
+        : `Error setting up the request: ${error.message}`;
+
+      res
+        .status(error.response ? error.response.status : 500)
+        .json({ error: errorMessage });
     });
 };
