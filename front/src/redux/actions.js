5af9d38bbeb39c20092ea635d64782b83c16e861
nicolaspratg <nicoprat@hotmail.com>
2024-01-30 22:01:57 -0300

    async await to addfav and removefav

--- front/src/redux/actions.js
+++ front/src/redux/actions.js
@@ -5,26 +5,48 @@ export const FILTER = "FILTER";
 export const ORDER = "ORDER";
 
 export const addFav = (character) => {
-  const endpoint = "http://localhost:3001/rickandmorty/fav";
-  return (dispatch) => {
-    axios.post(endpoint, character).then(({ data }) => {
+  return async (dispatch) => {
+    try {
+      const endpoint = "http://localhost:3001/rickandmorty/fav";
+      const { data } = await axios.post(endpoint, character);
       return dispatch({
         type: ADD_FAV,
         payload: data,
       });
-    });
+    } catch (error) {
+      console.error(error);
+      alert("Error al agregar a favoritos.");
+    }
   };
+  // axios.post(ENDPOINT, character).then(({ data }) => {
+  //   return dispatch({
+  //     type: ADD_FAV,
+  //     payload: data,
+  //   });
+  // });
 };
 export const removeFav = (id) => {
-  const endpoint = "http://localhost:3001/rickandmorty/fav/" + id;
-  return (dispatch) => {
-    axios.delete(endpoint).then(({ data }) => {
+  return async (dispatch) => {
+    try {
+      const endpoint = `http://localhost:3001/rickandmorty/fav/${id}`;
+      const { data } = await axios.delete(endpoint);
       return dispatch({
-        type: "REMOVE_FAV",
+        type: REMOVE_FAV,
         payload: data,
       });
-    });
+    } catch (error) {
+      console.error(error);
+      alert("Error al quitar de favoritos.");
+    }
   };
+  // return (dispatch) => {
+  //   axios.delete(`${ENDPOINT}/${id}`).then(({ data }) => {
+  //     return dispatch({
+  //       type: "REMOVE_FAV",
+  //       payload: data,
+  //     });
+  //   });
+  // };
 };
 export const filterCards = (gender) => {
   return {
