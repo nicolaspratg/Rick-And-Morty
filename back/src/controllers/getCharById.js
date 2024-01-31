3035aa672e80fc95a49e8d9a7b7f1e259da1c1d6
nicolaspratg <nicoprat@hotmail.com>
2024-01-30 21:56:22 -0300

    switched to async-await, try-catch

--- back/src/controllers/getCharById.js
+++ back/src/controllers/getCharById.js
@@ -2,38 +2,28 @@ const axios = require("axios");
 const BASE_URL = "https://rym2.up.railway.app/api/character/";
 const API_KEY = "pi-nicolaspratg";
 
-module.exports.getCharById = (req, res) => {
-  const { id } = req.params;
-  const URL = `${BASE_URL}${id}?key=${API_KEY}`;
+module.exports.getCharById = async (req, res) => {
+  try {
+    const charId = req.params.id;
+    const URL = `${BASE_URL}${charId}?key=${API_KEY}`;
+    const { data } = await axios.get(URL);
+    const { id, name, gender, species, origin, image, status } = data;
+    const character = {
+      id,
+      status,
+      name,
+      species,
+      origin,
+      image,
+      gender,
+    };
 
-  axios
-    .get(URL)
-    .then(({ data }) => {
-      const { id, name, gender, species, origin, image, status } = data;
-
-      const character = {
-        id,
-        status,
-        name,
-        species,
-        origin,
-        image,
-        gender,
-      };
-
-      res
-        .status(character.id ? 200 : 404)
-        .json(character.id ? character : { error: "Not Found" });
-    })
-    .catch((error) => {
-      const errorMessage = error.response
-        ? `Server responded with a non-2xx status code: ${error.response.status}`
-        : error.request
-        ? "No response received from the server"
-        : `Error setting up the request: ${error.message}`;
-
-      res
-        .status(error.response ? error.response.status : 500)
-        .json({ error: errorMessage });
-    });
+    res
+      .status(character.id ? 200 : 404) // obtiene char.id? status 200 sino 404
+      .json(character.id ? character : { error: "Not Found" }); // si esta el char.id devuelve json char, sino json error
+  } catch (error) {
+    res
+      .status(error.response ? error.response.status : 500)
+      .json({ error: error.message });
+  }
 };
