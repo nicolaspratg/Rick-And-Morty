87b3de454a9d1868b79ca675615f2a0229fce47a
nicolaspratg <nicoprat@hotmail.com>
2024-02-05 18:05:19 -0300

    fix to validatons at login

--- front/src/validations.js
+++ front/src/validations.js
@@ -1,20 +1,20 @@
 export function validateEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  !email
+  return !email
     ? "Email cannot be empty"
     : email.length > 34
-    ? "Email cannot have more than 35 characters."
+    ? "Email cannot have more than 35 characters"
     : !emailRegex.test(email)
-    ? "Invalid email format."
+    ? "Invalid email format"
     : null;
 }
 
 export function validatePassword(password) {
   const passwordRegEx = /^(?=.*\d).{6,10}$/;
 
-  !password
-    ? "Password cannot be empty."
+  return !password
+    ? "Password cannot be empty"
     : !passwordRegEx.test(password)
-    ? "Between 6-10 characters only."
+    ? "Between 6-10 characters only"
     : null;
 }
