a6ea3ca061647c0bad25f0e5a26ae3278a657b09
nicolaspratg <nicoprat@hotmail.com>
2025-07-21 17:28:19 +0200

    backend setup for deployment

--- back/src/index.js
+++ back/src/index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const server = require("./app");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
   console.log("Server raised in port: " + PORT);
